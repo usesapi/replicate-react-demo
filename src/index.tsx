94b8910d66c1d44d7dbf75b4877122433384a08a
yardenst <yardenst@voyantis.ai>
2023-01-23 12:30:46 -0800

    remove captacha

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import {init} from '@sapi/js-sdk';
-
 import App from './App';
 
-init({sapiId: process.env.REACT_APP_SAPI_ID, requireAuth : true});
+init({sapiId: process.env.REACT_APP_SAPI_ID});
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
