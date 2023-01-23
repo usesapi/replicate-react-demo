c100d49408a319b90bfb6aa1ef682864f0c8df2f
yardenst <yardenst@voyantis.ai>
2023-01-23 12:24:05 -0800

    design change

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { init } from '@sapi/js-sdk';
+import {init} from '@sapi/js-sdk';
 
 import App from './App';
 
-init({ sapiId: process.env.REACT_APP_SAPI_ID, requireAuth: true });
+init({sapiId: process.env.REACT_APP_SAPI_ID, requireAuth : true});
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
